af6f5875e2fb068d2162309ca2de401aee52e340
JK1331 <jakubkubala3@gmail.com>
2022-10-04 13:42:24 +0200

    Code refactoring

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,21 +1,27 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.8;
+pragma solidity ^0.8.7;
 
-import "./ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyNFT is ERC721 {
+contract BasicNFT is ERC721 {
     string public constant TOKEN_URI = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
-    function mint(address to, uint tokenId) external {
-        _mint(to, tokenId);
+    uint256 private s_tokenCounter;
+    constructor() ERC721("Doggie", "DOD") {
+        s_tokenCounter = 0;
     }
 
-    function burn(uint tokenId) external {
-        require(msg.sender == _ownerOf[tokenId], "MyNFT: Not owner");
-        _burn(tokenId);
+    function mintNFT() public returns (uint256) {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+        return s_tokenCounter;
     }
 
-    function tokenURI(uint256 /* tokenId */) public view returns (string memory) {
+    function tokenURI(uint256 /* tokenId */) public view override returns (string memory) {
         // require(_exists(tokenId))
         return TOKEN_URI;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
     }
 }
