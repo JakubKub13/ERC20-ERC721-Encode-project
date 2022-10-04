7598d495e5d3f84f52b49a4df7ac4af5995f19a2
JK1331 <jakubkubala3@gmail.com>
2022-10-04 12:42:03 +0200

    Working on ERC721 funcs

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -14,4 +14,24 @@ contract ERC721 is IERC721 {
     mapping(uint => address) internal _approvals;
     mapping(address => mapping(address => bool)) public isApprovedForAll;
 
+    function supportsInterface(bytes4 interfaceID) external view returns (bool) {
+        return interfaceID == type(IERC721).interfaceId || interfaceID == type(IERC165).interfaceId;
+    }
+
+/// Returns amount of NFTs owned by the address owner
+    function balanceOf(address owner) external view returns (uint balance) {
+        require(owner != address(0), "ERC721: Owner == address(0)");
+        return _balanceOf[owner];
+    }
+
+    function ownerOf(uint tokenId) external view returns (address owner) {
+        owner = _ownerOf[tokenId];
+        require(owner != address(0), "ERC721: Owner == address(0)");
+    }
+
+/// By calling this func caller sets the permission to operator to either take control of all of caller NFTs or to revoke that permission
+    function setApprovalForAll(address operator, bool _approved) external {
+        isApprovedForAll[msg.sender][operator] = _approved;
+        emit ApprovalForAll(msg.sender, operator, _approved);
+    }
 }
